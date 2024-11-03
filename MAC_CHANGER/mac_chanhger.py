5c220b5effe1758d4681084703e5e3b75b68c6aa
DevBhuva <145397562+DevBhuva@users.noreply.github.com>
2024-11-03 15:15:52 +0530

    Delete MAC_CHANGER directory

--- MAC_CHANGER/mac_chanhger.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-interface = input("interface >")
-new_mac= input("new MAC>")
-print("[+] Changing MAC address for" + interface + " to "+ new_mac)
-
-subprocess.call("ipconfig"+ interface +" down", shell=True)
-subprocess.call("ipconfig"+ interface +" hw ether "+ new_mac, shell=True)
-subprocess.call("ipconfig"+ interface +" up ", shell=True)
